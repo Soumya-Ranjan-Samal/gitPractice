3a6cd62bab45329740ce63a5228176a7318bd341
soumya-ranjan-samal <soumyaranjan1405@gmail.com>
2026-02-10 15:43:00 +0530

    added two routes

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ import express from "express";
 
 const app = express();
 
+app.get("/show/sum/:a/:b", (req,res)=>{
+    res.send(Number(req.params.a) + Number(req.params.b));
+});
+
+app.get("/show/:name", (req,res)=>{
+    let name = req.params.name;
+    console.log(name);
+    res.send(name);
+});
+
+app.get("/hello", (req,res)=>{
+    res.send('world');
+});
+
 app.get("/", (req, res)=>{
     res.send("Ok");
 });
+
+// new modification in index.js
 
 app.listen(3000, ()=>{
     console.log("server started liteing...");
