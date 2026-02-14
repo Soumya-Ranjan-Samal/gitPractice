7d674326549100a694f566cb118884ad70f6b981
soumya-ranjan-samal <soumyaranjan1405@gmail.com>
2026-02-14 12:42:47 +0530

    Separated admin and user rate limit

--- index.js
+++ index.js
@@ -10,26 +10,34 @@ const apiReqLimitUser = rateLimit({
     legacyHeaders: false,
 });
 
-app.use(apiReqLimit);
+const apiReqLimitAdmin = rateLimit({
+    windowMs: 1000 * 60 * 5, // five minute
+    limit: 3,
+    message: "Too many request",
+    standardHeaders : true,
+    legacyHeaders: false,
+});
 
-app.get("/show/sum/:a/:b", (req,res)=>{
+// app.use(apiReqLimit);
+
+app.get("/show/sum/:a/:b", apiReqLimitUser, (req,res)=>{
     let {a = 0,b = 0} = req.params;
     res.send(Number(a) + Number(b));
 });
 
 // new comment line
 
-app.get("/show/:name", (req,res)=>{
+app.get("/show/:name", apiReqLimitUser, (req,res)=>{
     let name = req.params.name;
     console.log(name);
     res.send(name);
 });
 
-app.get("/hello", (req,res)=>{
+app.get("/hello", apiReqLimitUser, (req,res)=>{
     res.send('world');
 });
 
-app.get("/", (req, res)=>{
+app.get("/", apiReqLimitAdmin, (req, res)=>{
     console.log("Hello this line got added in the default route");
     res.send("Ok");
 });
